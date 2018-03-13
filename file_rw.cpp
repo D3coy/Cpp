ed74dc0ecaed34a1838f4b6628cbb9fd51e89f43
Decoy <dorito@email.ru>
2018-03-14 00:27:46 +0300

    Add files via upload

---
+++ file_rw.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main(int argc, char *argv[])       //argc = arg.count(кол-во аргументов) ; argv = arg.vector(указатель на массив строковых параметров) >=1
+{
+	char text[50];                     //переменная хранящая считанные символы из файла text.txt, буфер ->50
+	/*
+	ofstream file;                     //объект класса записи в файл
+	file.open("text.txt");             //открыть -> создать файл
+	file << "work with files in c++";  //запись в файл
+	file.close();                      //закрытие файла
+	*/
+	
+	ifstream file("text.txt");       //создали объект класса чтения из файла и открыли на чтение
+	if (!file.is_open())               //проверка не открыт(например не существует больше)
+	{
+		cout << "Error, no file here.." << "\n";
+	}
+	else
+	{
+		file.getline(text, 50);
+		cout << text << "\n";
+		file.close();
+	}
+	
+	cin.get();
+	return 0;
+}
