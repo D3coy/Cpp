dc7aa69738315ebbb7d705c0db3091c8926ecbd2
Decoy <dorito@email.ru>
2018-03-21 06:17:56 -0400

    Update func_overload.cpp

--- func_overload.cpp
+++ func_overload.cpp
@@ -3,6 +3,19 @@
 
 using namespace std;
 
+void print(string word);            //prototype
+void print(float word);
+float del(float a, float b);
+
+int main()
+{
+  string text = "hello";
+  print(text);                      //string print()
+
+  float res = del(22, 0);
+  print(res);                       //float print()
+}
+
 void print(string word)            //string only
 {
   cout << word << endl;
@@ -13,19 +26,19 @@ void print(float word)              //float only
   cout << word << endl;
 }
 
+
 float del(float a, float b)
 {
-  if(b == 0)
-    return -1;
-  else
+  try {                              //checking :0
+    if(b == 0){
+     throw(b);
+   }
+    else{
       return a / b;
-}
-
-int main()
-{
-  string text = "hello";            //string print()
-  print(text);
-
-  float res = del(22, 10);
-  print(res);                       //float print()
+    }
+  }
+  catch(float err_b){
+    cout << "Division by zero is forbidden" << endl;
+    return 0;
+  }
 }
